8ae49dd2da6ebaee8f7f7e82523fde09867552cb
hoanglongle77 <100249743+hoanglongle77@users.noreply.github.com>
2024-04-22 18:30:56 +0700

    New update in student

--- COMP1640-WebDev/Models/Contribution.cs
+++ COMP1640-WebDev/Models/Contribution.cs
@@ -16,9 +16,11 @@ namespace COMP1640_WebDev.Models
         [ForeignKey("AcademicYear")]
         public string AcademicYearId { get; set; } = string.Empty;
         public AcademicYear AcademicYear { get; set; }
+
         public string Title { get; set; } = string.Empty;
         public string Document { get; set; } = string.Empty;
         public byte[] Image { get; set; }
+
         public DateTime SubmissionDate { get; set; }=DateTime.Now;
         public bool IsEnabled { get; set; }
         public bool IsSelected { get; set; }
